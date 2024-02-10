84631bc4a989289105f16c9ef3324f528dc1e06b
MauResendez <mauricioresendez524@gmail.com>
2024-02-10 14:33:26 -0600

    Created types for items

---
+++ src/types/index.ts
@@ -0,0 +1,70 @@
+export type Channel = {
+  id: string,
+  game_id: string,
+  game_name: string,
+  is_mature: boolean
+  language: string,
+  started_at: string,
+  tag_ids: [],
+  tags: string[],
+  thumbnail_url: string,
+  title: string,
+  type: string,
+  user_id: string,
+  user_login: string,
+  user_name: string,
+  viewer_count: number,
+}
+
+export type Badge = {
+  pk: string;
+  sk: string;
+  name: string;
+  versions: object;
+}
+
+export type Emote = {
+  pk: string,
+  sk: string,
+  format: string[],
+  images: object,
+  name: string,
+  scale: string[]
+  themes: string[],
+}
+
+export type Clip = {
+  pk: string,
+  sk: string,
+  broadcaster_id: string,
+  broadcaster_name: string,
+  created_at: string,
+  creator_id: string,
+  creator_name: string,
+  embed_url: string,
+  game_id: string
+  language: string,
+  thumbnail_url: string,
+  title: string,
+  url: string,
+  video_id: string,
+  view_count: number,
+}
+
+export type Stream = {
+  id: string,
+  game_id: string,
+  game_name: string,
+  is_mature: boolean
+  language: string,
+  started_at: string,
+  tag_ids: [],
+  tags: string[],
+  thumbnail_url: string,
+  title: string,
+  type: string,
+  user_id: string,
+  user_login: string,
+  user_name: string,
+  viewer_count: number,
+}
