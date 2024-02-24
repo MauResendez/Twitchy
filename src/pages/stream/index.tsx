ca182f81c174600e9165b0e103cf0eaec8947865
MauResendez <mauricioresendez524@gmail.com>
2024-02-24 03:40:08 -0600

    Added action buttons for stream page

--- src/pages/stream/index.tsx
+++ src/pages/stream/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@app/components/ui/button';
 import { Icons } from '@app/components/ui/spinner';
 import { useQuery } from '@tanstack/react-query';
-import { TwitchPlayer } from 'react-twitch-embed';
+import { ExternalLinkIcon, PlayIcon, UserIcon } from 'lucide-react';
+import { TwitchEmbed } from 'react-twitch-embed';
 
 const Stream = () => {
-  const { isPending, error, data } = useQuery({
+  const { isPending, error, data, refetch } = useQuery({
     queryKey: ['stream'],
     queryFn: () => {
       const streams = JSON.parse(sessionStorage.getItem("streams") as any);
@@ -17,12 +19,30 @@ const Stream = () => {
 
   if (isPending) return <Icons.spinner className="h-20 w-20 animate-spin" />
 
-  if (error) return 'An error has occurred: ' + error.message
-
-  if (data.length == 0) return "We're sorry. We couldn't find a stream for you."
+  if (error || !data) return "We're sorry. We couldn't find a stream for you."
   
   return (
-    <TwitchPlayer channel={data.user_login} autoplay muted={false} />
+    <div className="flex flex-1 flex-col items-center justify-center space-y-4">
+      <TwitchEmbed channel={data.user_login} autoplay muted={false} />
+      <div className="flex items-center space-x-4">
+        <Button size="sm" variant="default" onClick={() => refetch()}>
+          <PlayIcon className="mr-2 h-4 w-4" />
+          Find a new stream
+        </Button>
+        <a href={`channels/${data.user_login}`} target="_blank">
+          <Button size="sm" variant="default">
+            <UserIcon className="mr-2 h-4 w-4" />
+            View Channel
+          </Button>
+        </a>
+        <a href={`https://www.twitch.tv/${data.user_login}`} target="_blank">
+          <Button size="sm" variant="default">
+            <ExternalLinkIcon className="mr-2 h-4 w-4" />
+            Open on Twitch
+          </Button>
+        </a>
+      </div>
+    </div>
   );
 }
 
